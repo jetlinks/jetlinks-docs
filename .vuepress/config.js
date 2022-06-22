63dce98380058f760fb438cde58290c02c08e327
zhouhao <zh.sqy@qq.com>
2022-06-22 18:55:29 +0800

    增加协议开发手册

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -94,7 +94,17 @@ module.exports = {
                 ]
             },
             {
-                title: '开发手册',
+                title: '协议包开发手册',
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 2,    // 可选的, 默认值是 1
+                children:[
+                    ['/protocol/start.md', '介绍'],
+                    ['/protocol/first.md', '快速开始'],
+                    ['/protocol/mqtt.md', 'MQTT协议解析']
+                ]
+            },
+            {
+                title: '平台开发手册',
                 collapsable: false, // 可选的, 默认值是 true,
                 sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
